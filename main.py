d767af889647033dd0385a2def88cffb6c2c1f00
Ashwagandha-coder <max.cacz@yandex.ru>
2022-04-24 21:10:53 +0500

    commit

--- main.py
+++ main.py
@@ -4,13 +4,21 @@ db = sqlite3.connect("port.db")
 
 c = db.cursor()
 
+# Создание таблицы
+
 #c.execute("""CREATE TABLE articles (
 #title_text text,
 #full_text text,
 #views integer,
 #avtor text""")
 
-c.execute("INSERT INTO articles VALUES ('Google is cool!', 'Google is realy cool', 100, 'Admin')")
+# Добавление значений в таблицу
+
+#c.execute("INSERT INTO articles VALUES ('Facebook is cool!', 'Meta is realy cool', 50, 'Mora')")
+
+# Выборка значений
+
+
 
 
 db.commit()
