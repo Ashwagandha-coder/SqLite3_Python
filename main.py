5cc2bfa7546dc9ef0eacd364528932e335767cc3
Ashwagandha-coder <max.cacz@yandex.ru>
2022-04-24 20:51:55 +0500

    create table

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 import sqlite3
 
 db = sqlite3.connect("port.db")
+
+c = db.cursor()
+
+c.execute("""CREATE TABLE articles (
+title_text text,
+full_text text,
+views integer,
+avtor text""")
+
+
+
+
+db.close()
