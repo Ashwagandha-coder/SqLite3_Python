010992f6f72d7f8b3bb8e55e82e4b379963cc25d
Ashwagandha-coder <max.cacz@yandex.ru>
2022-04-24 21:45:55 +0500

    add delete and update date

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ c = db.cursor()
 # Создание таблицы
 
 #c.execute("""CREATE TABLE articles (
-#title_text text,
+#title text,
 #full_text text,
 #views integer,
 #avtor text""")
@@ -16,15 +16,21 @@ c = db.cursor()
 
 #c.execute("INSERT INTO articles VALUES ('Amazon is cool!', 'Amazon is realy cool', 50, 'Mora')")
 
+#Удаление данных
+c.execute("DELETE FROM articles WHERE avtor = 'Admin'")
 
+
+# Изменение данных
+c.execute("UPDATE articles SET avtor = 'Admin' WHERE title = 'Amazon is cool!'")
 
 # Выборка значений
 c.execute("SELECT rowid, * FROM articles WHERE rowid < 5 ORDER BY views DESC")
 items = c.fetchall()
-
+print(items)
+'''
 for el in items:
     print(el[1] + "\n" + el[4])
-
+'''
 
 db.commit()
 
