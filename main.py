9ee191959542c550a56f9eda9a64a6a65ad5cc14
Ashwagandha-coder <max.cacz@yandex.ru>
2022-04-24 21:00:58 +0500

    add values in table articles

--- main.py
+++ main.py
@@ -4,12 +4,16 @@ db = sqlite3.connect("port.db")
 
 c = db.cursor()
 
-c.execute("""CREATE TABLE articles (
-title_text text,
-full_text text,
-views integer,
-avtor text""")
+#c.execute("""CREATE TABLE articles (
+#title_text text,
+#full_text text,
+#views integer,
+#avtor text""")
 
+c.execute("INSERT INTO articles VALUES ('Google is cool!', 'Google is realy cool', 100, 'Admin')")
+
+
+db.commit()
 
 
 
